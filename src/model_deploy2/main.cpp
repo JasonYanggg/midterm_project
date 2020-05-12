06522e916e3404ffe9f3d2d47365aa0290d5aedb
JasonYang <jason53240@gmail.com>
2020-05-12 16:41:32 +0800

    2

--- src/model_deploy2/main.cpp
+++ src/model_deploy2/main.cpp
@@ -3,6 +3,8 @@
 #include "accelerometer_handler.h"
 #include "config.h"
 #include "magic_wand_model_data.h"
+#include "DA7212.h"
+#include <cmath>
 
 #include "tensorflow/lite/c/common.h"
 #include "tensorflow/lite/micro/kernels/micro_ops.h"
@@ -14,21 +16,24 @@
 #define N (3) // number of songs
 #define bufferLength (32)
 
+DA7212 audio;
+int16_t waveform[kAudioTxBufferSize];
 uLCD_4DGL uLCD(D1, D0, D2);
 int show;
 int modes;
 int songs;
-char name[3][20] = {"little star", "little bee", "do"};
+char name[3][20] = {"little star", "little bee", "happy birthday"};
 Thread t;
-// Thread song_t;
+Thread song_t;
 // Thread disp_t;
 EventQueue queue(32 * EVENTS_EVENT_SIZE);
-// EventQueue song_queue(32 * EVENTS_EVENT_SIZE);
+EventQueue song_queue(32 * EVENTS_EVENT_SIZE);
 // EventQueue disp_queue(32 * EVENTS_EVENT_SIZE);
 InterruptIn pause(SW2);
 // DigitalIn pause(SW2);
 InterruptIn button(SW3);
-int idC = -1;
+Timer debounce;
+int idC = 0;
 Serial pc(USBTX, USBRX);
 int length;
 int song[100];
@@ -38,6 +43,10 @@ char serialInBuffer[bufferLength];
 int serialCount = 0;
 int gesture;
 int enterrr = 0;
+int index_note;
+int len_note;
+bool play = false;
+int score = 0;
 
 // Create an area of memory to use for input, output, and intermediate arrays.
 // The size of this will depend on the model you're using, and may need to be
@@ -64,25 +73,33 @@ TfLiteStatus setup_status = SetupAccelerometer(error_reporter);
 // Whether we should clear the buffer next time we fetch data
 bool should_clear_buffer = false;
 
-// void play_song(void)
-// {
-//   int i = 0, j = 0;
-  
-//   while (true) {
-//     i = noteLength[j];
-//     while (i > 0) {
-//       uLCD.locate(0, 0);
-//       uLCD.printf("%d", song[j]);
-//       wait(1.0);
-//       i--;
-//     }
-//     j++;
-//     if (j == length) {
-//       wait(1.0);
-//       j = 0;
-//     }
-//   } 
-// }
+void playNote(int freq)
+{
+  for (int i = 0; i < kAudioTxBufferSize; i++)
+  {
+    waveform[i] = (int16_t) (sin((double)i * 2. * M_PI/(double) (kAudioSampleFrequency / freq)) * ((1<<16) - 1));
+  }
+  // the loop below will play the note for the duration of 1s 
+  audio.spk.play(waveform, kAudioTxBufferSize);
+}
+
+// play song and stop song
+void play_song(void)
+{
+  int i;
+  while (play) {
+    for (i = 0; play && i < length; i++) {
+      int j = noteLength[i];
+      while (play && j--) {
+        for (int k = 0; k < kAudioSampleFrequency / kAudioTxBufferSize; k++) {
+          song_queue.call(playNote, song[i]);
+        }
+        if (j >= 0) wait(1.0);
+      }
+    }
+    wait(1.0);
+  }
+}
 
 // Return the result of the last prediction
 int PredictGesture(float* output) {
@@ -156,7 +173,7 @@ void get_gest(void)
 
     // Produce an output
     if (gesture_index < label_num) {
-      error_reporter->Report(config.output_message[gesture_index]);
+      // error_reporter->Report(config.output_message[gesture_index]);
       // return gesture_index;
       gesture = gesture_index + 1;
       break;
@@ -164,40 +181,97 @@ void get_gest(void)
   }
 }
 
-void display(void)
+void display(int x)
 {
-  uLCD.cls();
-  if (show == 0) {
-    uLCD.color(BLUE);
-    uLCD.printf("Press SW2 to \n  select mode");
-  }
-  else if (show == 1) {
-    uLCD.color(BLUE);
-    uLCD.printf("\n  forward\n");
-    uLCD.printf("\n  backward\n");
-    uLCD.printf("\n  change songs\n");
-    uLCD.locate(0, 2 * modes - 1);
-    uLCD.printf(">");
-  }
-  else if (show == 2) {
-    uLCD.color(BLUE);
-    uLCD.printf("\n  %s\n", name[0]);
-    uLCD.printf("\n  %s\n", name[1]);
-    uLCD.printf("\n  %s\n", name[2]);
-    uLCD.locate(0, 2 * songs - 1);
-    uLCD.printf(">");
-  }
-  else if (show == 3) {
-    uLCD.circle(64, 50, 40, WHITE);
-    uLCD.triangle(45, 25, 45, 75, 95, 50, WHITE);
-    uLCD.color(BLUE);
-    uLCD.locate(0, 13);
-    if (!finish) {
-      uLCD.printf("Loading:\n  %s", name[songs - 1]);
+  if (x == -1) {
+    uLCD.cls();
+    if (show == 0) {
+      uLCD.color(BLUE);
+      uLCD.text_width(1);
+      uLCD.text_height(1);
+      uLCD.printf("User manual\n\n");
+      uLCD.printf("1. Press SW2 to \n  select mode\n\n");
+      uLCD.printf("2. Move clockwise\n  to scroll down\n\n");
+      uLCD.printf("3. Move counter \n  clockwise to\n  scroll up\n\n");
+      uLCD.printf("4. Press SW3 to \n  to select");
     }
-    else {
-      uLCD.printf("Now playing:\n  %s", name[songs - 1]);  
+    else if (show == 1) {
+      uLCD.color(BLUE);
+      uLCD.text_width(1);
+      uLCD.text_height(1);
+      uLCD.printf("\n  forward\n");
+      uLCD.printf("\n  backward\n");
+      uLCD.printf("\n  change songs\n");
+      uLCD.printf("\n  Taiko game\n");
+      uLCD.locate(0, 2 * modes - 1);
+      uLCD.printf(">");
     }
+    else if (show == 2) {
+      uLCD.color(BLUE);
+      uLCD.text_width(1);
+      uLCD.text_height(1);
+      uLCD.printf("\n  %s\n", name[0]);
+      uLCD.printf("\n  %s\n", name[1]);
+      uLCD.printf("\n  %s\n", name[2]);
+      uLCD.locate(0, 2 * songs - 1);
+      uLCD.printf(">");
+    }
+    else if (show == 3) {
+      uLCD.color(BLUE);
+      uLCD.text_width(1);
+      uLCD.text_height(1);
+      uLCD.circle(64, 50, 40, WHITE);
+      uLCD.triangle(45, 25, 45, 75, 95, 50, WHITE);
+      uLCD.color(BLUE);
+      uLCD.locate(0, 13);
+      if (!finish) {
+        uLCD.printf("Loading:\n  %s", name[songs - 1]);
+      }
+      else {
+        uLCD.printf("Now playing:\n  %s", name[songs - 1]);  
+      }
+    }
+    else if (show == 4) {
+        uLCD.color(BLUE);
+        uLCD.text_width(2);
+        uLCD.text_height(2);
+        uLCD.printf(" -Taiko- \n");
+        uLCD.line(0, 20, 130, 20, BLUE);
+        uLCD.filled_circle(0, 70, 20, WHITE);
+        uLCD.circle(0, 70, 16, 0xFF7F50);
+        uLCD.circle(40, 70, 9, 0x696969);
+        uLCD.line(5, 60, 20, 45, 0xA52A2A);
+        uLCD.line(5, 80, 20, 95, 0xA52A2A);
+        uLCD.line(5, 45, 25, 45, 0xA52A2A);
+        uLCD.line(5, 95, 25, 95, 0xA52A2A);
+        // uLCD.text_width(1);
+        // uLCD.text_height(1);
+        // uLCD.locate(4, 12);
+        uLCD.text_width(4);
+        uLCD.text_height(4);
+        uLCD.locate(1, 3);
+        uLCD.color(RED);
+        uLCD.printf("%.3d", score);
+        uLCD.rectangle(25, 93, 113, 127, 0xFFFF00);
+        uLCD.filled_circle(50, 35, 8, 0xDC143C);
+        uLCD.circle(70, 35, 7, 0xDC143C);
+        uLCD.triangle(75, 33, 79, 33, 77, 37, 0xDC143C);
+        uLCD.filled_circle(95, 35, 8, 0x800080);
+        uLCD.circle(115, 35, 7, 0x800080);
+        uLCD.triangle(120, 37, 124, 37, 122, 33, 0x800080);
+    }
+  }
+  else {
+    uLCD.locate(0, 1);
+    uLCD.printf(" ");
+    uLCD.locate(0, 3);
+    uLCD.printf(" ");
+    uLCD.locate(0, 5);
+    uLCD.printf(" ");
+    uLCD.locate(0, 7);
+    uLCD.printf(" ");
+    uLCD.locate(0, 2 * x - 1);
+    uLCD.printf(">");
   }
 }
 // song selection
@@ -205,39 +279,36 @@ void song_selection(void)
 {
   int j = 0;
 
-  pc.printf("enter song selection\r\n");
-
   // select song
   if (modes == 1) {
+    songs++; 
+    if (songs > N) {
+      songs = 1;
+    }    
+  }
+  else if (modes == 2) {
     songs--;
     if (songs == 0) {
       songs = N;
     }
   }
-  else if (modes == 2) {
-    songs++;
-    if (songs > N) {
-      songs = 1;
-    }
-  }
-  else if (modes == 3) {
-    songs = 1;
+  else if (modes == 3 || modes == 4) {
     finish = 0;
     show = 2;
-    display();
+    display(-1);
     while (!finish) {
       gesture = 0;
       get_gest();
       if (gesture == 1) {
         if (songs < N) {
           songs++;
-          display();
+          display(songs);
         }
       }
       else if (gesture == 2) {
         if (songs > 1) {
           songs--;
-          display();
+          display(songs);
         }
       }
     }
@@ -247,8 +318,7 @@ void song_selection(void)
   finish = 0;
   serialCount = 0;
   show = 3;
-  display();
-  /*
+  display(-1);
   pc.printf("%d\r\n", songs);
   while (!finish) {
     if (pc.readable()) {
@@ -285,58 +355,74 @@ void song_selection(void)
         j++;
       }
   }
-  display();*/
+  if (modes == 4)
+    show = 4;
+  display(-1);
+}
+
+void stop_play_song(void)
+{
+  // queue.cancel(idC);
+  play = false;
+  for (int i = 0; i < kAudioTxBufferSize; i++)
+  {
+    waveform[i] = (int16_t) 0;
+  }
+  // the loop below will play the note for the duration of 1s 
+  audio.spk.play(waveform, kAudioTxBufferSize);
 }
 
 // mode selection
 void mode_selection(void)
 {
-  pc.printf("enter mode selection\r\n");
-  // if (idC != -1)
-  //   song_queue.cancel(idC);
   modes = 1;
   finish = 0;
   show = 1;
-  display();
+  display(-1);
   while (!finish) {
     gesture = 0;
     get_gest();
-    pc.printf("%d\r\n", gesture);
-    if (gesture == 2) {
-      if (modes < 3) {
+    if (gesture == 1) {
+      if (modes < 4) {
         modes++;
-        display();
+        display(modes);
       }
     }
-    else if (gesture == 1) {
+    else if (gesture == 2) {
       if (modes > 1) {
         modes--;
-        display();
+        display(modes);
       }
     }
     wait(0.1);
   }
   song_selection();
-  // idC = song_queue.call(play_song);
-
-  pc.printf("leave mode selection\r\n");
+  play = true;
+  play_song();
 }
 
 // when select is pressed
-void button_press(void) {finish = !finish;}
+void button_press(void)
+{
+  if (debounce.read_ms() > 1000) {
+    finish = !finish;
+    debounce.reset();
+  }
+}
 
 int main(int argc, char* argv[]) {
-  pc.printf("restart\r\n");
-  show = 0;
+  show = 4;
   modes = 1;
   songs = 1;
   t.start(callback(&queue, &EventQueue::dispatch_forever));
-  // song_t.start(callback(&song_queue, &EventQueue::dispatch_forever));
+  song_t.start(callback(&song_queue, &EventQueue::dispatch_forever));
   // disp_t.start(callback(&disp_queue, &EventQueue::dispatch_forever));
   // disp_queue.call(get_gest);
-  pause.fall(queue.event(mode_selection));
+  pause.rise(queue.event(mode_selection));
+  pause.fall(song_queue.event(stop_play_song));
+  debounce.start();
   button.fall(&button_press);
-  display();
+  display(-1);
   
   if (model->version() != TFLITE_SCHEMA_VERSION) {
     error_reporter->Report(
@@ -351,40 +437,40 @@ int main(int argc, char* argv[]) {
   // An easier approach is to just use the AllOpsResolver, but this will
   // incur some penalty in code space for op implementations that are not
   // needed by this graph.
-    micro_op_resolver.AddBuiltin(
-        tflite::BuiltinOperator_DEPTHWISE_CONV_2D,
-        tflite::ops::micro::Register_DEPTHWISE_CONV_2D());
-    micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_MAX_POOL_2D,
-                                tflite::ops::micro::Register_MAX_POOL_2D());
-    micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_CONV_2D,
-                                tflite::ops::micro::Register_CONV_2D());
-    micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_FULLY_CONNECTED,
-                                tflite::ops::micro::Register_FULLY_CONNECTED());
-    micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_SOFTMAX,
-                                tflite::ops::micro::Register_SOFTMAX());
-    micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_RESHAPE,
-                                tflite::ops::micro::Register_RESHAPE(), 1);
+  micro_op_resolver.AddBuiltin(
+      tflite::BuiltinOperator_DEPTHWISE_CONV_2D,
+      tflite::ops::micro::Register_DEPTHWISE_CONV_2D());
+  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_MAX_POOL_2D,
+                              tflite::ops::micro::Register_MAX_POOL_2D());
+  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_CONV_2D,
+                              tflite::ops::micro::Register_CONV_2D());
+  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_FULLY_CONNECTED,
+                              tflite::ops::micro::Register_FULLY_CONNECTED());
+  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_SOFTMAX,
+                              tflite::ops::micro::Register_SOFTMAX());
+  micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_RESHAPE,
+                              tflite::ops::micro::Register_RESHAPE(), 1);
 
  // Allocate memory from the tensor_arena for the model's tensors
 interpreter->AllocateTensors();
 
 model_input = interpreter->input(0);
   
-  if ((model_input->dims->size != 4) || (model_input->dims->data[0] != 1) ||
-      (model_input->dims->data[1] != config.seq_length) ||
-      (model_input->dims->data[2] != kChannelNumber) ||
-      (model_input->type != kTfLiteFloat32)) {
-    error_reporter->Report("Bad input tensor parameters in model");
-    return -1;
-  }
+if ((model_input->dims->size != 4) || (model_input->dims->data[0] != 1) ||
+    (model_input->dims->data[1] != config.seq_length) ||
+    (model_input->dims->data[2] != kChannelNumber) ||
+    (model_input->type != kTfLiteFloat32)) {
+  error_reporter->Report("Bad input tensor parameters in model");
+  return -1;
+}
 
-  input_length = model_input->bytes / sizeof(float);
+input_length = model_input->bytes / sizeof(float);
 
-  
-  if (setup_status != kTfLiteOk) {
-    error_reporter->Report("Set up failed\n");
-    return -1;
-  }
-  // error_reporter->Report("Set up successful...\n");
-  
+
+if (setup_status != kTfLiteOk) {
+  error_reporter->Report("Set up failed\n");
+  return -1;
+}
+// error_reporter->Report("Set up successful...\n");
+
 }
